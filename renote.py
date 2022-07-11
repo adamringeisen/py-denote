2ad968670baab15989de7013dc9f5837ae7f2052
Adam Ringeisen <ringeisen@gmail.com>
2022-07-11 17:43:57 -0400

    Misc Fixes

--- renote.py
+++ renote.py
@@ -1,12 +1,16 @@
+"""CLI library."""
 import click
 from pathlib import Path
+
 # import subprocess
 import os
 from note import Note
 
-notes_directory: Path = Path(os.path.join(Path.home(), 'notes'))
+notes_directory: Path = Path(os.path.join(Path.home(), "notes"))
+
 
 def get_or_create_dir(dir_path: Path):
+    """Get or creates a directory."""
     if dir_path.exists():
         return dir_path
     else:
@@ -15,16 +19,18 @@ def get_or_create_dir(dir_path: Path):
 
 
 @click.command()
-@click.option('--format', default="mdYaml", help="Format of resulting note file")
-@click.option('--title', prompt="Title", help="Title of note")
-@click.option('--tags', prompt="Tags", help='Tags for note')
-def main(format, title, tags):  
-    note = Note(format, title, tags)  
+@click.option("--format", default="mdYaml", help="Format of resulting note file")
+@click.option("--title", prompt="Title", help="Title of note")
+@click.option("--tags", prompt="Tags", help="Tags for note")
+def main(format, title, tags):
+    """Create note from arguments."""
+    note = Note(format, title, tags)
     notes_dir = get_or_create_dir(notes_directory)
     path = notes_dir / note.fileId
-    with open(path, 'w') as file:
+    with open(path, "w") as file:
         note.print_front_matter(note.format, file)
     os.system(f"xdg-open {path}")
-   
+
+
 if __name__ == "__main__":
     main()
