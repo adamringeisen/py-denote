7c942bb994296684a438fcade3e2feeebe122baa
Adam Ringeisen <ringeisen@gmail.com>
2022-09-07 17:45:03 -0400

    Config file working, os removed

--- renote.py
+++ renote.py
@@ -2,38 +2,40 @@
 import click
 from pathlib import Path
 import configparser
-import os
 from note import Note
 
-notes_directory: Path = Path(os.path.join(Path.home(), "notes"))
+
+default_notes_directory: Path = Path.home() / "notes"
+config_directory: Path = Path(click.get_app_dir("renote"))
+config_file: Path = config_directory / "config.ini"
 
 
-def generate_default_config(path: Path):
-    """Generate a default configuration file."""
+def generate_default_config() -> configparser.ConfigParser:
+    """Generate and return default configuration file."""
     config = configparser.ConfigParser()
     config['Options'] = {
+        '# Availible Formats': 'mdYaml, mdToml, txt, org',
         'NoteFormat': 'mdYaml',
-        'NoteDirectory': notes_directory.as_posix()
+        'NoteDirectory': default_notes_directory.as_posix()
     }
-    with open(os.path.join(path, "config.ini"), 'w') as configfile:
+    with open(config_file) as configfile:
         config.write(configfile)
+    config.read(config_file)
+    return config
 
 
-def get_config():
+def get_config() -> configparser.ConfigParser:
     """Get the config file."""
-    config_dir = Path(click.get_app_dir("renote"))
-    if config_dir.exists():
-        print("Dir exists!")
+    config = configparser.ConfigParser()
+    if not config_file.exists():
+        return generate_default_config()
     else:
-        config_dir.mkdir()
-        generate_default_config(config_dir)
+        config.read(config_file)
+        return config
 
 
-def get_or_create_dir(dir_path: Path):
-    """Get or creates a directory.
-
-    Will be replaced by config at some point.
-    """
+def get_or_create_dir(dir_path: Path) -> Path:
+    """Get or creates a directory."""
     if dir_path.exists():
         return dir_path
     else:
@@ -49,10 +51,10 @@ def get_or_create_dir(dir_path: Path):
 @click.option("--tags", prompt="Tags", help="Tags for note")
 def main(format, title, tags):
     """Create note from arguments."""
-    get_config()
-    # note = Note(format, title, tags)
-    # notes_dir = get_or_create_dir(notes_directory)
-    # note.create_and_open_note(notes_dir)
+    config = get_config()
+    note = Note(config['Options']['NoteFormat'], title, tags)
+    notes_dir = get_or_create_dir(Path(config['Options']['NoteDirectory']))
+    note.create_and_open_note(notes_dir)
 
 
 if __name__ == "__main__":
